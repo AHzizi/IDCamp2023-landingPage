39f7df0dd2c06d6618e08acea58bcf38f6feda32
AHzizi <miftakulazizi0@gmail.com>
2023-10-09 10:53:13 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      'quicksand' : ["quicksand", "sans-serif"]
+    },
+  },
+  plugins: [],
+}
